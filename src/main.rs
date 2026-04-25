d61a2ec9b6442d8dda21949146e9aef60d4f9747
LilPatr1ck <sergeev.r.s@edu.mirea.ru>
2026-04-25 11:41:29 +0300

    input handle and program exiting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Калькулятор\nВведите выражение в формате (<число> <операция> <число>) или 0 для выхода.");
+
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Ошибка чтения");
+
+        let input = input.trim();
+        println!("Ваш ввод:{}",input);
+
+        if input == "0" {
+            println!("Выход из программы.");
+            break;
+        }
+    }
 }
